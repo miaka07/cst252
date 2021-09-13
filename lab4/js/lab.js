402fbb88134e396dc26ec20ac274e85cc04dbd4f
Marisia Alcaraz <77997297+miaka07@users.noreply.github.com>
2021-09-13 21:50:16 +0000

    js

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+/**
+ * Summary.
+ * 
+ * Description
+ * 
+ * @link URL
+ * @file This file defines that MyClass class.
+ * @author MiaAlcaraz
+ * @since 09.13.2021
+ */
+
+// Open the trunk
+  // Take out the jack & spare tire
+    // Jack up the car
+      // Take off the flat tire
+        // Put on the spare tire
+         // Lower the jack
+          // Replace jack & put tire in trunk
+            // Close trunk
