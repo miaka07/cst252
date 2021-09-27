42ee1722e6df97c042b8bdf98c7b78972de8dcf5
miaka07 <77997297+miaka07@users.noreply.github.com>
2021-09-27 15:21:18 -0700

    8

---
+++ lab8/js/lab8.js
@@ -0,0 +1,12 @@
+/*
+  Author: Mia Alcaraz & Raven Jackson
+  Created: 09.26.2021
+*/
+
+
+function main() {
+    // get element by id
+    outputElement = document.getElementById("js-output");
+    // change contents of element
+    outputElement.innerHTML = "Yo, 'sup?";
+}
