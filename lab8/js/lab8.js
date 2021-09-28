4658616b61e05615983a78fd4af73b87c6dd48eb
Marisia Alcaraz <77997297+miaka07@users.noreply.github.com>
2021-09-28 17:09:44 +0000

    lab8

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -4,9 +4,23 @@
 */
 
 
-function main() {
-    // get element by id
-    outputElement = document.getElementById("js-output");
-    // change contents of element
-    outputElement.innerHTML = "Yo, 'sup?";
+function isEven(x) {
+  return (x % 2 == 0);
 }
+
+//create an array
+array = [7, 17, 70, 71, 700]
+
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even?" , isEven(2));
+
+var result = array.map(isEven);
+// should return [false, false, true, false, true]
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x){
+  return x ** 0.5;
+})
+// should return [2.6457513110645907, 4.123105625617661, 8.366600265340756, 8.426149773176359, 26.457513110645905]
+console.log("Squareroot of array:", result);
