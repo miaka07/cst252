e551f94c444c291781649c51bf9ea6b460e8f6f8
miaka07 <77997297+miaka07@users.noreply.github.com>
2021-10-06 12:55:02 -0700

    10

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -3,59 +3,28 @@
   Created: 10.05.2021
 */
 
-// shuffleArray - take an array and shuffle the contents
-// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+var button = document.getElementById('my-button');
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+function sortText(text) {
+	return text.split('').sort().reverse().join('');
 }
 
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
-function reorderUserName(word) {
-    var wordArray = word.toLowerCase().split('');
-    var newArray = shuffleArray(wordArray);
-    return newArray.join('');
-}
 
-// given a string, return string in Title Case
-// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
-}
-
-// find the button element
-buttonEl = document.getElementById("my-button");
-console.log("button element:", buttonEl);
-// find the form element
-inputEl = document.getElementById("user-name");
-console.log("input element:", inputEl);
-// find output element
-outputEl = document.getElementById("output");
-console.log("output element:", outputEl);
-
-// add an event listener to button
-buttonEl.addEventListener("click", function(){
-  // get value from name element
-  var userName = inputEl.value;
-  // modify value - either sort or shuffle
-  var newName = toTitleCase(reorderUserName(userName));
-  // put value in output element
-  outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
+// Create an event handler within the JS file that listens for a button click.
+button.addEventListener('click', function() {
+	// When the button is pushed, prompt the user to input their name with the prompt() function.
+  //var name = prompt("What's your name?");
+  // find the value of the input field
+  var inputEl = document.getElementById('input');
+  var name = inputEl.value;
+  // run through sort
+  var newName = sortText(name);
+  // Use this input to change the text from ‘Hello, World!’ to ‘Hello’ + the user’s name
+  var newText = "Hello, " + newName + "!";
+  // find the element!
+	var heading = document.getElementById('greet');
+  // modify the heading text
+  heading.innerHTML = newText;
+  // change output
+  document.getElementById('output').innerHTML = newText;
 });
